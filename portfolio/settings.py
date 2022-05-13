5b22b79307b39524a8378a05e9fdf2f670046a17
Luis Filipe Carrete <lfcsanchez@live.com>
2022-05-13 11:33:42 -0300

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -74,14 +74,20 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'tasks',
+#        'USER': 'cloud',
+#        'PASSWORD': 'cloud',
+#        'HOST': '192.168.8.244',
+#        'PORT': '5432',
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tasks',
-        'USER': 'cloud',
-        'PASSWORD': 'cloud',
-        'HOST': '192.168.8.244',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
     }
 }
 
